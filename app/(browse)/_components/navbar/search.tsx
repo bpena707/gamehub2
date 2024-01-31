596e917937d41091de0df4432205ffb50996cd26
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-01-31 09:37:40 -0800

    search
    completed the search bar and url functionality. now search displays on the url and redirects to the /search route

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -3,21 +3,53 @@
 import qs from 'query-string'
 import { SearchIcon, X } from "lucide-react";
 import { useRouter } from "next/navigation";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export const Search = () => {
 
+    const router = useRouter()
+    const [ value, setValue ] = useState('')
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (!value) return
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: { term: value }
+        }, {skipEmptyString: true}) //?term= is not added to the url if value is empty
+
+        router.push(url) //localhost:3000/?term=value
+    }
+
+    const onClear = () => {
+        setValue('')
+        router.push('/')
+    }
+
   return (
       <form
-          onSubmit={() => {}}
+          onSubmit={onSubmit}
           className='relative w-full lg:w-[400px] flex items-center'
       >
           <Input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
             placeholder='search'
+            className='rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible: ring-offset-0'
           />
+          {value && (
+              <X
+                  className='absolute top-2.5 right-14 h-5 w-5 cursor-pointer text-muted-foreground hover:opacity-75 transition'
+                  onClick={onClear}
+              />
+          )}
           <Button
             type='submit'
+            size='sm'
+            variant='secondary'
             className='rounded-l-none'
           >
                 <SearchIcon className='h-5 w-5 text-muted-foreground' />
