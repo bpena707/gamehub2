862730bc77e3b0f6bbbe24c506da85b83c255595
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-02-02 23:02:58 -0800

    Update toggle.tsx
    updated the toggle so that users can expand and collapse sidebar

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import {useSidebar} from "@/store/use-sidebar";
+import {Button} from "@/components/ui/button";
+import {ArrowLeftFromLine, ArrowRightFromLine} from "lucide-react";
 
 export const Toggle = () => {
     const {
@@ -15,11 +17,27 @@ export const Toggle = () => {
 
   return (
       <>
+          {collapsed && (
+              <div className='hidden lg:flex w-full items-center justify-center pt-4 mb-4'>
+                  <Button
+                      variant='ghost'
+                      className='p-2 h-auto'
+                      onClick={onExpand}>
+                      <ArrowRightFromLine className='h-4 w-4 '/>
+                  </Button>
+              </div>
+          )}
           {!collapsed && (
               <div className='p-3 pl-6 mb-2 flex items-center w-full'>
-                <p>
+                <p className='font-semibold text-primary'>
                     for you
                 </p>
+                  <Button
+                      variant='ghost'
+                      className='ml-auto p-2 h-auto'
+                      onClick={onCollapse}>
+                      <ArrowLeftFromLine className='h-4 w-4 '/>
+                  </Button>
               </div>
           )}
       </>
