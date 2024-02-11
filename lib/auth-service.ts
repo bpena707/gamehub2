41b9dccb982f1d2e0bb8e10bca45a04e60e9ee6e
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-02-11 00:33:10 -0800

    Update auth-service.ts
    added the get by username to get the username of the current logged in user

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -23,3 +23,29 @@ export const getSelf = async () => {
 
     return user;
 }
+
+// load creator dashboard by usering current username
+export const getSelfByUsername = async (username: string) => {
+    const self = await currentUser();
+
+    if (!self || !self.username) {
+        throw new Error("Unauthorized");
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            username,
+        }
+    })
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    // if the user is not the same as the current user, throw an error to prevent unauthorized access to other users dashboard
+    if (self.username !== user.username) {
+        throw new Error("Unauthorized");
+    }
+
+    return user;
+}
