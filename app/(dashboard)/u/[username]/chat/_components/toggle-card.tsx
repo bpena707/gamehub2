1747a140a8bd04bd892f72aa81f89f5f3e418583
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-02-16 20:18:14 -0800

    updated chat stream
    now the user can toggle and update the status of the chat from on to off and the corresponding changes are made on the database

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -2,6 +2,10 @@
 
 import {Switch} from "@/components/ui/switch";
 
+import {updateStream} from "@/actions/stream";
+import {toast} from "sonner";
+import {useTransition} from "react";
+
 type FieldTypes = 'isChatEnabled' | 'isChatDelayed' | 'isChatFollowersOnly'
 
 interface ToggleCardProps {
@@ -15,6 +19,17 @@ const ToggleCard = ({
     value = false,
     field
                     }: ToggleCardProps) => {
+
+    // update the stream with the new value without blocking the UI
+    const [isPending, startTransition] = useTransition()
+    const onChange = async () => {
+        startTransition(() => {
+            updateStream({ [field]: false })
+                .then(() => toast.success('Chat settings updated'))
+                .catch(() => toast.error('Failed to update chat settings'))
+        })
+    }
+
   return (
       <div className='rounded-xl bg-muted p-6'>
           <div className='flex items-center justify-between'>
@@ -22,7 +37,11 @@ const ToggleCard = ({
                   {label}
               </p>
               <div className='space-y-2'>
-                  <Switch checked={value}>
+                  <Switch
+                      disabled={isPending}
+                      onCheckedChange={onChange}
+                      checked={value}
+                  >
                       {value ? 'On' : 'Off'}
                   </Switch>
               </div>
