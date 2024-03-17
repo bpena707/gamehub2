be7144301733f39fd450d334c4742e56d471293e
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-03-16 22:20:26 -0700

    Update video.tsx
    now based on the different parameters the video will or will not stream based on if the actual obs service is streaming or not. series of checks then render the result on the video component window

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -1,9 +1,6 @@
 'use client'
 import {ConnectionState, Track} from "livekit-client"
-import {
-    useConnectionState,
-    useRemoteParticipant
-} from "@livekit/components-react"
+import {useConnectionState, useRemoteParticipant, useTracks} from "@livekit/components-react"
 
 interface VideoProps {
     hostName: string
@@ -11,9 +8,31 @@ interface VideoProps {
 }
 
 export const Video = ({hostName, hostIdentity}: VideoProps) => {
+    // displays connection status
+    const connectionState = useConnectionState()
+    // displays name of the participant
+    const participant = useRemoteParticipant(hostIdentity)
+    // uses the source track and filters to find the identity of the participant
+    const tracks = useTracks([
+        Track.Source.Camera,
+        Track.Source.Microphone
+    ]).filter((track) => track.participant.identity === hostIdentity)
+
+    let content
+
+    // different scenarios based on if the participant is connected or not
+    if (!participant && connectionState === ConnectionState.Connected){
+        content = <p>Host is offline</p>
+    }else if (!participant || tracks.length === 0) {
+        content = <p>Loading...</p>
+    }else {
+        content = <p>Live video</p>
+    }
+
+
   return (
       <div className='aspect-video border-b group relative'>
-          Video
+          {content}
       </div>
   )
 }
