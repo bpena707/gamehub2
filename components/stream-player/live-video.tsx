8956effa43965ada63438419e33fb868535c8d47
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-03-18 14:17:02 -0700

    full screen control
    working on the fullscreen control that appears while the user is streaming. so far worked on the button

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -5,6 +5,7 @@
 import {Participant, Track} from "livekit-client";
 import {useRef} from "react";
 import {useTracks} from "@livekit/components-react";
+import {FullscreenControl} from "@/components/stream-player/fullscreen-control";
 
 interface LiveVideoProps {
     participant: Participant
@@ -28,6 +29,14 @@ export const LiveVideo = ({participant}: LiveVideoProps) => {
   return(
       <div ref={wrapperRef} className='relative h-full flex'>
           <video ref={videoRef} width='100%' />
+          <div className='absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all'>
+              <div className='absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4'>
+                  <FullscreenControl
+                      isFullscreen={false}
+                      onToggle={() => {}}
+                  />
+              </div>
+          </div>
       </div>
   )
 }
