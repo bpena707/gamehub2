d388be54eaa754e2f22257217faf1631b69ad0d1
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-03-20 15:53:04 -0700

    chat compontnet
    the chat component is where comments will render and is collapsable. styling was applied to make it show to the right and adjust on expand

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import {useMediaQuery} from "usehooks-ts";
+import {useChatSidebar} from "@/store/use-chat-sidebar";
+import {useChat, useConnectionState, useRemoteParticipant} from "@livekit/components-react";
+import {ConnectionState} from "livekit-client";
+import {useEffect, useMemo, useState} from "react";
+import {ChatHeader} from "@/components/stream-player/chat-header";
+
+interface ChatProps {
+    hostName: string
+    hostIdentity: string
+    viewerName: string
+    isFollowing: boolean
+    isChatEnabled: boolean
+    isChatDelayed: boolean
+    isChatFollowersOnly: boolean
+}
+
+export const Chat = ({
+    hostName,
+    hostIdentity,
+    viewerName,
+    isFollowing,
+    isChatEnabled,
+    isChatDelayed,
+    isChatFollowersOnly
+}: ChatProps) => {
+    const matches = useMediaQuery('(max-width)')
+
+    const {onExpand, variant} = useChatSidebar((state) => state)
+    // connection state and participant is from livekit and checks connection for chat and the identity of the participant
+    const connectionState = useConnectionState()
+    const participant = useRemoteParticipant(hostIdentity)
+
+    // is online if the participant and the connection are on
+    const isOnline = participant && connectionState === ConnectionState.Connected
+
+    const isHidden = !isChatEnabled || !isOnline
+
+    // chatMessages are remapped to messages
+    const [value, setValue] = useState("")
+    const {chatMessages: messages, send} = useChat()
+
+    // reset collapse state based on match queries
+    useEffect(() => {
+        if (matches) onExpand()
+
+    }, [matches, onExpand]);
+
+  //reverse messages in order to render them newest messages are shown at the bottom
+    const reversedMessages = useMemo(() => {
+        return messages.sort((a,b) => b.timestamp - a.timestamp)
+    }, [messages]);
+
+    const onSubmit = () => {
+        if (!send) return
+
+        send(value)
+        setValue("")
+    }
+
+    const onChange = (value: string) => {
+        setValue(value)
+    }
+
+  return(
+      <div className='flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]'>
+          <ChatHeader
+
+          />
+      </div>
+  )
+}
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -4,6 +4,10 @@ import {Stream, User} from "@prisma/client";
 import {useViewerToken} from "@/hooks /use-viewer-token";
 import { LiveKitRoom } from "@livekit/components-react";
 import {Video} from "@/components/stream-player/video";
+import {useChatSidebar} from "@/store/use-chat-sidebar";
+import {cn} from "@/lib/utils"
+import {Chat} from "@/components/stream-player/chat";
+import {ChatToggle} from "@/components/stream-player/chat-toggle";
 
 interface StreamPlayerProps {
     user: User & {stream: Stream | null}
@@ -18,6 +22,9 @@ export const Index = ({
 }: StreamPlayerProps) => {
 const {token, name, identity} = useViewerToken(user.id)
 
+    // gets the whole state
+    const { collapsed } = useChatSidebar((state) => state)
+
     console.log({token, name, identity})
 
     if (!token || !name) {
@@ -26,15 +33,38 @@ const {token, name, identity} = useViewerToken(user.id)
 
     return (
         <>
+            {collapsed && (
+                <div className='hidden lg:block fixed top-[100] right-2 z-50'>
+                    <ChatToggle />
+                </div>
+            )}
            <LiveKitRoom
                serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
                token={token}
-               className='grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full'
+               className={cn(
+                   'grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full',
+                   collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 "
+
+                   )}
            >
                <div className='space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10'>
                    <Video
                     hostName={user.username}
                     hostIdentity={user.id}
+                   />
+               </div>
+               <div className={cn(
+                   'col-span-1',
+                   collapsed && 'hidden'
+               )} >
+                   <Chat
+                       viewerName={name}
+                       hostName={user.username}
+                       hostIdentity={user.id}
+                       isFollowing={isFollowing}
+                       isChatEnabled={stream.isChatEnabled}
+                       isChatDelayed={stream.isChatDelayed}
+                       isChatFollowersOnly={stream.isChatFollowersOnly}
                    />
                </div>
            </LiveKitRoom>
